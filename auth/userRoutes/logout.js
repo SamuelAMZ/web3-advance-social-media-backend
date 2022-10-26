65ca5ac929683acc6850dfb5b39b0588871b59d0
sam_dev <samueldevpro09@gmail.com>
2022-10-27 00:28:18 +0200

    set cors for logout route

--- auth/userRoutes/logout.js
+++ auth/userRoutes/logout.js
@@ -2,6 +2,13 @@ const express = require("express");
 const LogoutRoute = express.Router();
 
 LogoutRoute.get("/", (req, res) => {
+  res.set({
+    "Access-Control-Allow-Origin": process.env.DOMAIN,
+    "Access-Control-Allow-Credentials": true,
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Origin, Content-Type, Accept",
+  });
+
   res.cookie("uToken", "", { maxAge: 1 });
   res.status(200).json({ message: "logout successfully", code: "ok" });
 });
