2cc25d5419592fc165390019a4cac99736e6573f
sam_dev <samueldevpro09@gmail.com>
2022-12-14 21:50:39 +0100

    login with username

--- auth/userRoutes/login.js
+++ auth/userRoutes/login.js
@@ -9,38 +9,56 @@ const bcrypt = require("bcrypt");
 const { createToken } = require("./jwt");
 
 const schema = Joi.object({
-  email: Joi.string().email().lowercase().required(),
+  emailOrUsername: Joi.string().lowercase().required(),
   password: Joi.string().max(1024).required(),
 });
 
 userLoginRoute.post("/", async (req, res) => {
-  const { email, password } = req.body;
+  const { emailOrUsername, password } = req.body;
 
   try {
     // joi validation sbody data
-    const validation = await schema.validateAsync({ email, password });
+    const validation = await schema.validateAsync({
+      emailOrUsername,
+      password,
+    });
   } catch (error) {
     res.status(400).json({ message: error.details[0].message });
     return;
   }
 
   // validation
-  if (!email || !password) {
+  if (!emailOrUsername || !password) {
     res.status(400).json({ message: "verify your inputs" });
     return;
   }
 
-  // check for the email address in the db
-  const checkUser = await User.findOne({ email });
+  // push data to it if username or email found
+  const verifier = [];
+  let userFound;
 
-  if (checkUser === null || !checkUser) {
-    res.status(400).json({ message: "email not found" });
+  // check for the email address in the db
+  const checkUser = await User.findOne({ email: emailOrUsername });
+  if (checkUser) {
+    verifier.push(1);
+    userFound = checkUser;
+  }
+
+  // check for the username in the db
+  const checkUsername = await User.findOne({ username: emailOrUsername });
+  if (checkUsername) {
+    verifier.push(1);
+    userFound = checkUsername;
+  }
+
+  if (verifier.length === 0) {
+    res.status(400).json({ message: "email or username not found" });
     return;
   } else {
     // dehash pass and try to match them
-    if (await bcrypt.compare(password, checkUser.password)) {
+    if (await bcrypt.compare(password, userFound.password)) {
       // generate user token
-      const token = createToken(checkUser.id);
+      const token = createToken(userFound.id);
       // pass token to cookie
       res.cookie("uToken", token, {
         maxAge: 24 * 60 * 60 * 1000,
@@ -52,11 +70,11 @@ userLoginRoute.post("/", async (req, res) => {
       res.status(200).json({
         message: "login successfully",
         code: "ok",
-        id: checkUser.id,
-        name: checkUser.name,
+        id: userFound.id,
+        name: userFound.name,
       });
     } else {
-      res.status(400).json({ message: "verify password" });
+      res.status(400).json({ message: "verify username, email or password" });
       return;
     }
   }
